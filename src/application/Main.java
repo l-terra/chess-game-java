8093872dc84d0d7cedbda76e42c6f22754d435a4
Lucas Terra <lucas.terra8587@gmail.com>
2025-02-22 22:24:10 -0300

    added board, piece and position classes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,11 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
-
-
-
+        Board board = new Board(8, 8);
     }
 }

577269536dd36521cf25b78394c37574f330c74c
Lucas Terra <lucas.terra8587@gmail.com>
2025-02-27 19:32:52 -0300

    players turns

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initalSetup();
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 
     public ChessPiece[][] getPieces() {
@@ -37,6 +49,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateNextPosition(source, next);
         Piece capturedPiece = makeMove(source, next);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -48,10 +61,13 @@ public class ChessMatch {
     }
 
     private void validateSourcePosition(Position position) {
-        if(!board.thereIsAPiece(position)) {
+        if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on this source position.");
         }
-        if(!board.piece(position).isThereAnyPossibleMove()) {
+        if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+            throw new ChessException("The chosen piece is not yours.");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There are no possible moves for the chosen piece.");
         }
     }
@@ -60,6 +76,11 @@ public class ChessMatch {
         if(!board.piece(source).possibleMove(next)) {
             throw new ChessException("The chosen piece can't be moved to this position.");
         }
+    }
+
+    private void nextTurn() {
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
